b03d8a3a10b528b65d01dbcae708e2462fbbcb11
Gracrys <gracrys@gmail.com>
2020-07-26 15:37:58 -0430

    some others details

--- src/App.svelte
+++ src/App.svelte
@@ -17,11 +17,36 @@
 		padding: 0 20%;
 		box-sizing: border-box;
 		justify-content: center;
+		h3{
+			color: hsl(565, 45%, 55% );
+			margin-top: auto;
+			}
 		footer{
 			display: flex;
 			margin-bottom: auto;
+			button{
+				padding: 0.5rem;
+				background: transparent;
+				border: 2px solid  hsl(565, 45%, 55% );
+				color:  hsl(565, 45%, 55% );
+				font-weight: bold;
+				margin-top: 0.5rem;
+				cursor: pointer;
+				&:hover{
+					background: hsl(565, 45%, 55% );
+					box-shadow: 2px 2px 0 hsla(565, 45%, 55%, 0.3 );
+					color: black;
+				}
+			}
 			button:nth-of-type(1){
 				flex: 1;
+				margin-right: 0.5rem;
+			}
+			#tweet-quote, a:visited{
+				color:  hsl(565, 45%, 55% );
+				&:hover{
+					color: black;
+				}
 			}
 		
 		}
@@ -34,23 +59,27 @@
 		width: 100%;
 		display: block;
 		border: none;
-		box-shadow: inset -4px -4px 0 hsla(265, 45%, 0%, 0.3 );
+		box-shadow: inset -4px -4px 0 #a7a7a74d, inset 4px 4px 0 hsla(265, 55%, 0%, 0.3 );
 		border-radius: 0.6rem;
 		position: relative;
-		margin-bottom: 0;
+		margin: 0;
 		box-sizing: border-box;
+		background: hsl(265, 45%, 35% );
+
 	}
 </style>
 
 <template>
 	<section id="quote-box" class="o-card">
+		<h3>Random Genre Generator</h3>
 		<dialog id="author">
 			{#await data}
 				<blockquote>
 					waiting..			
 				</blockquote>
 			{:then genre}
-				<h1>{genre[counter]}</h1>
+				<h1 id="text">{genre[counter]}</h1>
+				<em id="author">binaryjazz</em>
 			{:catch error}
 				<blockquote>
 					Oh no!
@@ -59,7 +88,7 @@
 		</dialog>
 		<footer>
 			<button id="new-quote" on:click={() => counter >= 19 ? counter=0 : counter++  }>New quote</button>
-			<button id="tweet-quote">popo</button>
+			<button id="tweet-quote"><a href="">&#x1F3B6;</a></button>
 		</footer>
 	</section>
 </template>
